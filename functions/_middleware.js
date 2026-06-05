1935b7e1f2941568319c365101a3854600b659cb
jldsnetbr <jldsnetbr@github.com>
2026-06-05 09:42:38 -0300

    fix: MIME type via middleware por extensao do arquivo

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,6 +2,16 @@ const SUPABASE_ORIGIN = 'https://sfpilqfqkuzqyswgyolx.supabase.co';
 
 const API_PATHS = ['/auth/v1/', '/rest/v1/', '/storage/v1/', '/realtime/v1/'];
 
+const MIME_TYPES = {
+  js: 'text/javascript',
+  css: 'text/css',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  ico: 'image/x-icon',
+  json: 'application/json',
+  webmanifest: 'application/manifest+json',
+};
+
 export async function onRequest(context) {
   const { request, next } = context;
   const url = new URL(request.url);
@@ -9,7 +19,19 @@ export async function onRequest(context) {
   const isApiRequest = API_PATHS.some(path => url.pathname.startsWith(path));
 
   if (!isApiRequest) {
-    return next();
+    const response = await next();
+    const ext = url.pathname.split('.').pop();
+    const contentType = MIME_TYPES[ext];
+    if (contentType) {
+      const headers = new Headers(response.headers);
+      headers.set('Content-Type', contentType);
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers,
+      });
+    }
+    return response;
   }
 
   const proxyUrl = new URL(url.pathname + url.search, SUPABASE_ORIGIN);
